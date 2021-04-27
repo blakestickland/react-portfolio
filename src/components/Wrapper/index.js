2918d7e693403d75bc22a16fed8c8f97ae323fbb
Blake Sticland <blakelists@yahoo.com.au>
2021-04-28 00:29:23 +1000

    Updates

--- src/components/Wrapper/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./wrapper.css";
-
-function Wrapper(props) {
-  return <main style={{margin:"4% auto", alignItems:"center"}}>{props.children}</main>;
-}
-
-export default Wrapper;
