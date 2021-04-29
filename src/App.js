9daf9bae7f11c71a85814884bac5e60d564dc250
Blake Sticland <blakelists@yahoo.com.au>
2021-04-29 21:56:58 +1000

    try with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
 import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -9,19 +9,20 @@ import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
 function App() {
-  return (
-    <Router>
-          <Header />
-          <Wrapper>
-            <Route exact path="/" component={About} />
-            <Route exact path="/react-portfolio" component={About} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/portfolio" component={Portfolio} />
-            <Route exact path="/contact" component={Contact} />
-          </Wrapper>
-          <Footer />
-    </Router>
-  );
+    return (
+        <Router basename="/react-portfolio">
+            <Header />
+            <Wrapper>
+                <Switch>
+                    <Route exact path="/" component={About} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/portfolio" component={Portfolio} />
+                    <Route exact path="/contact" component={Contact} />
+                </Switch>
+            </Wrapper>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
